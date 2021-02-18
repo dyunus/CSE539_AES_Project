89310d13caa117bd0e4bd2fabff9b55cd4588576
Danial Yunus <dyunus@asu.edu>
2021-02-17 21:25:54 -0700

    init commit

---
+++ CSE539_AES_Project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CSE539_AES_Project
+//
+//  Created by Danial Yunus on 2/17/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
